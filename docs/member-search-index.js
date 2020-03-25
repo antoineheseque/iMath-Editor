391abd8322ed58af7dbb73f02b9456497ba0d5de
Antoine <antoine.heseque@isen.yncrea.fr>
2020-03-25 21:25:07 +0100

    Hello world! + Javadoc

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"fr.iMath","c":"Main","l":"Main()","url":"%3Cinit%3E()"},{"p":"fr.iMath","c":"Main","l":"main(String[])","url":"main(java.lang.String[])"}]
---
+++ src/fr/iMath/Main.java
@@ -0,0 +1,17 @@
+package fr.iMath;
+
+/**
+ * Main class. This is where everything begin!
+ * @author HESEQUE Antoine
+ * @author JEANNIN Louis
+ */
+public class Main {
+	/**
+	 * Main function
+	 * @param args arguments given right after executing the file.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Word!");
+	}
+
+}
