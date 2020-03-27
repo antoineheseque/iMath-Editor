08a7e3cd091fe4c873e05453132f49551d76bd84
Antoine <antoine.heseque@isen.yncrea.fr>
2020-03-27 19:51:26 +0100

    Basic deployment - Folder and packages preparation - Show Window

--- src/fr/iMath/Main.java
+++ src/fr/iMath/Main.java
@@ -1,17 +1,37 @@
 package fr.iMath;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Main class. This is where everything begin!
  * @author HESEQUE Antoine
  * @author JEANNIN Louis
  */
-public class Main {
+public class Main extends Application{
 	/**
 	 * Main function
 	 * @param args arguments given right after executing the file.
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello Word!");
+		System.setProperty("file.encoding", "UTF-8");
+		launch(args);
 	}
 
+	@Override
+	public void start(Stage stage) throws Exception {
+		try {
+			Parent parent = FXMLLoader.load(getClass().getResource("/scenes/GraphUI.fxml"));
+			Scene scene = new Scene(parent);
+			stage.setScene(scene);
+			stage.setTitle("[v1.0.0] iMath : NodeEditor");
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
