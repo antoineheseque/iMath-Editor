8cb4303fa7a451b82d8ae497439f6869869f08d3
Louis JEANNIN <louis.jeannin@isen.yncrea.fr>
2020-03-28 20:52:27 +0100

    update of InputAnalyzerAlgorithm v2

--- src/fr/iMath/algorithms/InputAnalyzerAlgorithm.java
+++ src/fr/iMath/algorithms/InputAnalyzerAlgorithm.java
@@ -2,25 +2,41 @@ package fr.iMath.algorithms;
 
 import fr.iMath.objects.EquationObjectData;
 import fr.iMath.objects.Operator;
-
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+/**
+ * Input Analyser Algorithm
+ * @author JEANNIN Louis
+ *
+ */
 
 public class InputAnalyzerAlgorithm {
 	// Convert StringInput to a list of Numbers and Operations here ..
     private SortedMap<Integer, EquationObjectData> list = new TreeMap<>();
     private List<Integer> indexArray = new ArrayList<>();
 
+    /**
+     * Analyse.
+     * @param function String
+     * @return List of EquationObjectData
+     */
+
     public List<EquationObjectData> analyse(String function){
         matchesConstant(function,"[1-9]*");
         matchesOperator(function,"[()+\\/*\\-^~]");
         matchesChar(function,"[a-z]*");
-        System.out.println(function);
-        showList(getList(list));
-        showIndexArray();
+        //System.out.println(function);
+        //showList(getList(list));
         return getList(list);
     }
+
+    /**
+     * Update EquationObjectData list with functions and variables
+     * @param text String
+     * @param regex String
+     */
 
     public void matchesChar(String text, String regex) {
         Pattern pattern = Pattern.compile(regex);
@@ -43,6 +59,12 @@ public class InputAnalyzerAlgorithm {
         }
     }
 
+    /**
+     * Update EquationObjectData list with constants
+     * @param text String
+     * @param regex String
+     */
+
     public void matchesConstant(String text, String regex) {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
@@ -53,11 +75,11 @@ public class InputAnalyzerAlgorithm {
         }
     }
 
-    public void showIndexArray(){
-        for(int i : indexArray){
-            System.out.println(i);
-        }
-    }
+    /**
+     * Update  EquationObjectData list with operators
+     * @param text String
+     * @param regex String
+     */
 
     public void matchesOperator(String text, String regex) {
         Pattern pattern = Pattern.compile(regex);
@@ -99,6 +121,12 @@ public class InputAnalyzerAlgorithm {
         }
     }
 
+    /**
+     * EquationObjectData list analysis.
+     * @param list Map<Integer,EquationObjectData>
+     * @return List of EquationObjectData
+     */
+
     public List<EquationObjectData> getList(SortedMap<Integer,EquationObjectData> list){
         Set s = list.entrySet();
         Iterator i = s.iterator();
@@ -111,12 +139,15 @@ public class InputAnalyzerAlgorithm {
             if((!previous.getType().toString().equals("OPERATOR") || previous.getObject().toString().equals("RIGHTPARENTHESIS")) && (!value.getType().toString().equals("OPERATOR") || value.getObject().toString().equals("LEFTPARENTHESIS"))){
                 objectList.add(new EquationObjectData(Operator.MULTIPLY));
             }
-            //System.out.println("->" + previous.getObject().toString() + " et " + value.getObject().toString());
             objectList.add(value);
             previous = value;
         }
         return objectList;
     }
+
+    /**
+     * Show EquationObjectData list
+     */
 
     public void showList(List<EquationObjectData> list) {
         for (EquationObjectData e : list) {
